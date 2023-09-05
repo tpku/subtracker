ffc893035dc9a9a333828e14d3f835fa819501df
tpku <tpku@icloud.com>
2023-09-05 20:29:39 +0200

    Supabase connection and dependencies

---
+++ src/lib/initSupabase.js
@@ -0,0 +1,16 @@
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { createClient } from "@supabase/supabase-js";
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./constants";
+
+// const supabase = createClient(
+//     "https://hsspcjlmxksnfzlifbml.supabase.co",
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzc3BjamxteGtzbmZ6bGlmYm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MDU3NzYsImV4cCI6MjAwOTQ4MTc3Nn0.9D6HjzbKSptqMZnsBvxutFm-Vm0p0t24k85Ez_OuaAY",
+//     {
+//         auth: {
+//             storage: AsyncStorage,
+//             autoRefreshToken: true,
+//             persistSession: true,
+//             detectSessionInUrl: false,
+//         },
+//     }
+// );
