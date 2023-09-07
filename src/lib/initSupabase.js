c3f70c2cd9a152a340bb503dfbf385f8349ae893
tpku <tpku@icloud.com>
2023-09-07 15:11:50 +0200

    Delete fix

--- src/lib/initSupabase.js
+++ src/lib/initSupabase.js
@@ -12,17 +12,4 @@ const supabase = createClient(REACT_NATIVE_SUPABASE_URL, REACT_NATIVE_SUPABASE_A
     },
 });
 
-// const supabase = createClient(
-//     "https://hsspcjlmxksnfzlifbml.supabase.co",
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhzc3BjamxteGtzbmZ6bGlmYm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MDU3NzYsImV4cCI6MjAwOTQ4MTc3Nn0.9D6HjzbKSptqMZnsBvxutFm-Vm0p0t24k85Ez_OuaAY",
-//     {
-//         auth: {
-//             storage: AsyncStorage,
-//             autoRefreshToken: true,
-//             persistSession: true,
-//             detectSessionInUrl: false,
-//         },
-//     }
-// );
-
 export default supabase;
