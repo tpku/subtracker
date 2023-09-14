5fd070ac8ec5d96f67598738f25ffdeb7d1beb47
tpku <tpku@icloud.com>
2023-09-14 19:24:10 +0200

    Fetch services, subscriptions. Add new user subscriptions to supabase

--- src/screens/DashboardScreen/DashboardScreen.jsx
+++ src/screens/DashboardScreen/DashboardScreen.jsx
@@ -1,142 +1,175 @@
-import React, { useEffect, useState } from 'react'
-import supabase from '../../lib/initSupabase'
-import { View, Text, Alert, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from "react"
+import { View, Text, Alert, StyleSheet, Pressable } from "react-native"
 
-import CustomButton from '../../components/CustomButton/CustomButton'
-import InputField from '../../components/InputField/InputField'
-import Spinner from 'react-native-loading-spinner-overlay'
+import supabase from "../../lib/initSupabase"
+import CustomButton from "../../components/CustomButton/CustomButton"
+import InputField from "../../components/InputField/InputField"
+import Spinner from "react-native-loading-spinner-overlay"
 
-const DashboardScreen = (session) => {
+const DashboardScreen = ({ session }) => {
   const [loading, setLoading] = useState(false)
   const [authUser, setAuthUser] = useState([])
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [services, setServices] = useState('')
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [services, setServices] = useState(false)
   const [loggedInUser, setLoggedInUser] = useState({})
+  const [fetchedSubscriptions, setFetchedSubscriptions] = useState("")
+  const [selectedSubscription, setSelectedSubscriptionId] = useState("")
+
+  const currentDate = new Date()
+  const formattedDate = `${currentDate.getFullYear()}/${
+    currentDate.getMonth() + 1
+  }/${currentDate.getDate()}`
+  const initialSubscription = {
+    users_id: "",
+    subscriptions_id: "",
+    start_date: formattedDate, // If no start date is given todays date is set YYYY/MM/DD
+    discount_active: false,
+  }
+  const [newSubscription, setNewSubscription] = useState(initialSubscription)
 
   useEffect(() => {
-    const sessionUser = async () => {
+    const fetchUser = async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser()
       setAuthUser(user)
+      getUser()
+      if (authUser) fetchServices()
     }
-    sessionUser()
-
-    getUser()
+    fetchUser()
   }, [])
-  // console.log(authUser);
 
   const getUser = async () => {
     setLoading(true)
-    const { data: user, error } = await supabase.from('users').select('*')
+    const { data: user, error } = await supabase.from("users").select("*") // Should we change the select to name only or do we need the whole user object later?
     if (user) setLoggedInUser(user[0])
-
     if (error) Alert.alert(error.message)
     setLoading(false)
   }
 
-  //   Get services
-  //   const getServices = async () => {
-  //     setLoading(true)
-  //     const { data: services, error } = await supabase
-  //       .from('services')
-  //       .select('*')
-  //     // if (services) console.log(services);
-  //     if (services) setServices(services)
+  const fetchServices = async () => {
+    const { data: services, error } = await supabase
+      .from("services")
+      .select("*")
+    if (services) setServices(services)
+    if (error) Alert.alert(error.message)
+  }
 
-  //     if (error) Alert.alert(error.message)
-  //     setLoading(false)
-  //   }
+  const fetchSubscriptions = async (serviceId) => {
+    const { data: subscriptions, error } = await supabase
+      .from("subscriptions")
+      .select("id, name, price")
+      .eq("services_id", serviceId)
+    if (subscriptions) setFetchedSubscriptions(subscriptions)
+    if (error) Alert.alert(error.message)
+  }
 
-  // Get Service Subscriptions
-  //   const getServiceSubscriptions = async (serviceId) => {
-  //     setLoading(true)
-  //     let { data: subscriptions, error } = await supabase
-  //       .from('subscriptions')
-  //       .select('name, price')
-  //       .eq('services_id', serviceId)
-  //     if (subscriptions) console.log(subscriptions)
+  const updateSubscription = (userId, subscriptionId) => {
+    const updatedSubscription = {
+      ...initialSubscription,
+      users_id: userId,
+      subscriptions_id: subscriptionId,
+    }
+    setNewSubscription(updatedSubscription)
+  }
 
-  //     if (error) Alert.alert(error.message)
-  //     setLoading(false)
-  //   }
+  const addUserSubscription = async (subscription) => {
+    setLoading(true)
+    const { data, error } = await supabase
+      .from("users_subscriptions")
+      .insert([subscription])
+    if (!error) console.log("Successfully updated!")
+    if (error) Alert.alert(error.message)
+    setLoading(false)
+  }
 
-  const onLogoutPressed = async () => {
+  const logout = async () => {
     setLoading(true)
     const { error } = await supabase.auth.signOut()
-    // window.localStorage.clear()
-
-    setIsLoggedIn('loggedOut')
-    console.warn('Logout pressed')
     if (error) Alert.alert(error.message)
     setLoading(false)
   }
 
-  // Update Users table if uuid = users.id
-  //   const insertName = async (firstName, lastName) => {
-  //     setLoading(true)
-  //     let updateData = {}
-  //     if (firstName) updateData.first_name = firstName
-  //     if (lastName) updateData.last_name = lastName
-
-  //     const { error } = await supabase
-  //       .from('users')
-  //       .update(updateData)
-  //       .eq('id', authUser.id)
-  //       .select('*')
-  //     if (!error) console.log('Successfully updated!')
-  //     if (error) {
-  //       Alert.alert(error)
-  //     }
-
-  //     setLoading(false)
-  //   }
+  // Update user name in "public.users" if "uuid.id = users.id"  - MOVE TO USER SETTINGS SCREEN WITH CORRECT PROPS AND FETCHES
+  const updateUserName = async (firstName, lastName) => {
+    setLoading(true)
+    let updateData = {}
+    if (firstName) updateData.first_name = firstName
+    if (lastName) updateData.last_name = lastName
+    const { error } = await supabase
+      .from("users")
+      .update(updateData)
+      .eq("id", authUser.id)
+      .select("*")
+    if (!error) console.log("Successfully updated!")
+    if (error) Alert.alert(error)
+    setLoading(false)
+  }
 
   return (
     <View style={styles.root}>
       <Text style={styles.heading}>
         Welcome user: {loggedInUser.first_name}
       </Text>
-      <CustomButton text="Test Button" onPress={() => console.log(Pressed)} />
+      {/* <CustomButton text="Test Button" onPress={() => testSubs()} /> */}
+      <>
+        {services &&
+          services.map((service, index) => (
+            <CustomButton
+              text={service.name}
+              key={index}
+              btnType="SECONDARY"
+              onPress={() => {
+                fetchSubscriptions(service.id)
+              }}></CustomButton>
+          ))}
+      </>
+      <>
+        {fetchedSubscriptions &&
+          fetchedSubscriptions.map((sub, index) => (
+            <Pressable
+              key={index}
+              onPress={() => setSelectedSubscriptionId(sub.id)}>
+              <Text>{sub.name}</Text>
+            </Pressable>
+          ))}
+      </>
+
+      {/* <CustomButton
+        text="Update Subscription"
+        onPress={() => updateSubscription(authUser.id, selectedSubscription)}
+      /> */}
+
+      {/* <CustomButton
+        text="Add Subscription"
+        onPress={() => addUserSubscription(newSubscription)}
+      /> */}
+
       <CustomButton
         text="Logout"
-        onPress={onLogoutPressed}
-        btnType={'SECONDARY'}
-        isLoggedIn={'loggedIn'}
+        onPress={logout}
+        btnType={"SECONDARY"}
+        textType="TERTIARY"
+        isLoggedIn={"loggedIn"}
       />
 
-      {/* Update Users below, 2x InputFields, 1x CustomButton */}
+      {/* Update User information - MOVE TO USER SETTINGS SCREEN WITH CORRECT PROPS AND FETCHES */}
       {/* <InputField
         placeholder="First name"
         value={firstName}
         setValue={setFirstName}
       />
       <InputField
-        placeholder="Full name"
+        placeholder="Last name"
         value={lastName}
         setValue={setLastName}
       />
       <CustomButton
-        text="Insert name"
-        onPress={() => insertName(firstName, lastName)}
+        text="Update name"
+        onPress={() => updateUserName(firstName, lastName)}
       /> */}
 
-      {/* Render services if services === true and get subscriptions on click */}
-      {/* {services && services.length > 0
-        ? services.map((service, index) => {
-            return (
-              <CustomButton
-                key={index}
-                text={service.name}
-                onPress={() => {
-                  console.log(service?.name),
-                    getServiceSubscriptions(service.id)
-                }}
-              />
-            )
-          })
-        : ''} */}
       <View>
         <Spinner visible={loading} />
       </View>
@@ -147,13 +180,13 @@ const DashboardScreen = (session) => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "start",
     padding: 20,
-    backgroundColor: 'orangered',
+    backgroundColor: "orangered",
   },
   heading: {
     flex: 1,
-    textAlign: 'center',
+    textAlign: "start",
     fontSize: 10,
   },
 })
